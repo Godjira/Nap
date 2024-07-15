0adb6b4b558e70bffaa86c515552e3706cd67613
PashaDev2 <pasha.yakubovsky@lynksen.com>
2024-07-15 19:12:47 +0300

    +mob ai +hud

--- levels/Character0.gd
+++ levels/Character0.gd
@@ -35,39 +35,6 @@ func _ready():
 	# Because we use SubViewport node needs to get that from inner context
 	get_viewport_context()
 
-func _process(_delta):
-	for ray in raycasts:
-		var active_lines = []
-		if ray.is_colliding():
-			var r = ray as RayCast2D
-			for line in lines:
-				if line.get_point_position(1) == r.target_position:
-					line.default_color = Color.GREEN
-					active_lines.append(line)
-				else:
-					line.default_color = Color.RED
-
-func _draw():
-	if(raycasts.size() < 17):
-		for i in range(0, 16, 1):
-			# create a new RayCast2D node
-			var ray = RayCast2D.new()
-			ray.target_position = Vector2.from_angle(i * 60) * 20
-			ray.visible = true
-			ray.enabled = true
-			ray.collision_mask = 2
-			ray.exclude_parent = true
-			raycasts.append(ray)
-			# draw line from character to world
-			add_child(ray)
-			var line = Line2D.new()
-			add_child(line) # add the Line2D as a child of your CharacterBody2D
-			line.width = 0.15
-			line.add_point(Vector2.ZERO) # add start point
-			line.add_point(ray.target_position) # add end point
-			line.default_color = Color.RED # set the line color
-			lines.append(line)
-
 func get_viewport_context():
 	# Get all the child nodes of the sub_viewport.
 	var childrenNodes = sub_viewport.get_children()
@@ -174,20 +141,23 @@ func start():
 
 func _on_attract_body_entered(body):
 	if body.is_in_group("Enemy"):
-		body.attack_timer.start()
-
-
-func _on_attack_body_entered(body):
-	if body.is_in_group("Enemy"):
-		body.state = body.HIT
-
+		print("Mob attracted", body)
+		body.state = body.SURROUND
 
 func _on_attract_body_exited(body):
 	if body.is_in_group("Enemy"):
-		body.attack_timer.stop()
-		body.state = body.SURROUND
+		print("Mob stop be  attracted", body)
+		body.state = body.IDLE
+
+func _on_attack_body_entered(body):
+	if body.is_in_group("Enemy"):
+		print("Mob in attack area", body)	
+		body.state = body.HIT
+		body.attack_timer.start()
 
 
 func _on_attack_body_exited(body):
 	if body.is_in_group("Enemy"):
+		print("Mob stop attack", body)
 		body.state = body.SURROUND
+		body.attack_timer.stop()
